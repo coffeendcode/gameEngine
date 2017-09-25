11573c00eb55b8adf0b67bce4e61d1707ecc8ce5
Rich <rufhinator@hotmail.com>
2017-09-25 02:11:46 -0400

    Lab #2 submission
    Changelog:
    -Added cpu detection
    -Added RAM detection
    -Added HDD detection
    -Added functionality for only one program to be running at one time

--- gameEngine/Source/gameEngine/gameEngine/Source.cpp
+++ gameEngine/Source/gameEngine/gameEngine/Source.cpp
@@ -6,44 +6,53 @@
 #include<gameux.h>
 
 DWORD ReadCPUSpeed();
+bool checkHDD();
+bool checkRAM();
 void initInstance();
 
 using namespace std;
+
+class thisProgram{};
+
 int main() {
 
-	//std::cout << "Hello World!" << std::endl << std::endl;
+	//Check if this program is already running
+	CreateMutex(0, FALSE, "Local\\$myprogram$");
+	if (GetLastError() == ERROR_ALREADY_EXISTS)
+	{
+		cout << "Program is already running!" << endl << endl;
+		system("pause");
+		return 0;
+	}
+	else
+	{
+		cout << "Program Starting!" << endl << endl;
+	}
 
+	//Run initialization functions
 	initInstance();
 
+	//Pause before the program terminates
 	system("pause");
 	return 0;
 }
 
 void initInstance()
 {
+	//Output the cpu's speed
 	cout << "CPU: ";
-	cout << ReadCPUSpeed() << endl << endl;
-}
+	cout << ReadCPUSpeed() << "Ghz" << endl;
 
-bool IsOnlyInstance(LPCTSTR gameTitle)
-{
-	HANDLE handle = CreateMutex(NULL, TRUE, gameTitle);
+	//Check for available space
+	if (checkHDD == false)
+		cout << "No available space, please make some" << endl;
+	else
+		cout << "You have enough space" << endl;
 
-	if (GetLastError() != ERROR_SUCCESS)
-	{
-		HWND hWnd = FindWindow(gameTitle, NULL);
-		if (hWnd)
-		{
-			ShowWindow(hWnd, SW_SHOWNORMAL);
-			SetFocus(hWnd);
-			SetForegroundWindow(hWnd);
-			SetActiveWindow(hWnd);
-			return false;
-			std::cout << "Game is already Running";
-		}
-	}
-	std::cout << "Game is starting";
-	return true;
+	if (checkRAM() == false)
+		cout << "You dont have either the required physical or virtual RAM" << endl;
+	else
+		cout << "You have enough dedidated WAM!! Please wait..." << endl << endl;
 }
 
 DWORD ReadCPUSpeed() {
@@ -56,5 +65,37 @@ DWORD ReadCPUSpeed() {
 	{
 		RegQueryValueEx(hKey, "~MHz", NULL, &type, (LPBYTE)&dwMHz, &bufSize);
 	}
+	float Ghz = (float)dwMHz;
+	dwMHz = (Ghz / 1000.f);
 	return dwMHz;
+}
+
+bool checkHDD()
+{
+	__int64 available;
+	__int64 total;
+	__int64 free;
+	GetDiskFreeSpaceEx(NULL, (PULARGE_INTEGER)&available, (PULARGE_INTEGER)&total, (PULARGE_INTEGER)&free);
+
+	if ((available / (1024 * 1024)) < 200)
+		return false;
+	else
+		return true;
+
+}
+bool checkRAM()
+{
+	MEMORYSTATUSEX ram;
+	ram.dwLength = sizeof(ram);
+	GlobalMemoryStatusEx(&ram);
+
+	if (ram.ullTotalPhys < 100000)
+		return false;
+	else
+		return true;
+
+	if (ram.ullTotalVirtual < 600000)
+		return false;
+	else
+		return true;
 }
